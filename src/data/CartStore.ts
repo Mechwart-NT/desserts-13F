c38462ae6d27a1e6f03810733d8738c278a9f40d
1/5 - NT <you@example.com>
2026-09-22 09:38:53 +0200

    Refactor cart functionality to use Zustand for state management and update order confirmation flow

--- src/data/CartStore.ts
+++ src/data/CartStore.ts
@@ -8,15 +8,23 @@ type CartState = {
     decrementItem: (name: string) => void,
     removeItem: (name: string) => void,
     clearCart: () => void
-}
+} 
 
 export const useCartStore = create<CartState>((set)=>({
     items: [],
     addItem: (dessert) => set((state)=>({
         items: [...state.items, {...dessert, quantity: 1}]
     })),
-    incrementItem: (name) => set((state)=>({})),
-    decrementItem: (name) => set((state)=>({})),
-    removeItem: (name) => set((state)=>({})),
-    clearCart: () => set((state)=>({})),
+    incrementItem: (name) => set((state)=>({
+        items: state.items.map((item) => item.name === name ? {...item, quantity: item.quantity + 1} : item)
+    })),
+    decrementItem: (name) => set((state)=>({
+        items: state.items.map((item)=> item.name === name ? {...item, quantity: item.quantity - 1} : item)
+    })),
+    removeItem: (name) => set((state)=>({
+        items: state.items.filter((item) => item.name !== name)
+    })),
+    clearCart: () => set((state)=>({
+        items: []
+    })),
 }))
